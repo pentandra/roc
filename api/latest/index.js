1575e56578669d61e1b6fddd2d09db86182ec930
Chris Chapman <chris@pentandra.com>
2013-06-06 12:54:46 -0600

    Added a shell of scaladocs - trying to figure out how this works

---
+++ api/latest/index.js
@@ -0,0 +1,1 @@
+Index.PACKAGES = {"com" : [], "com.pentandra" : [], "com.pentandra.roc" : [], "com.pentandra.roc.api" : [{"trait" : "com\/pentandra\/roc\/api\/Case.html", "name" : "com.pentandra.roc.api.Case"}]};
